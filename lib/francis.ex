9644cdddb5eaf65a370bdd3def7eb8473df7002f
Filipe Cabaço <filipecabaco@gmail.com>
2025-06-25 21:44:43 +0100

    add helper functions json/text/html and improved error message on unhandled error

--- lib/francis.ex
+++ lib/francis.ex
@@ -119,6 +119,89 @@ defmodule Francis do
         |> halt()
       end
 
+      @doc """
+      Sends a JSON response with the given status code and data.
+      ## Examples
+      ```
+      json(conn, 201, %{message: "Success"})
+      ```
+      """
+      @spec json(Plug.Conn.t(), integer(), map() | list()) :: Plug.Conn.t()
+      def json(conn, status, data) do
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(status, Jason.encode!(data))
+      end
+
+      @doc """
+      Sends a JSON response with a 200 status code and the given data.
+      ## Examples
+      ```
+      json(conn, %{message: "Success"})
+      ```
+      """
+      @spec json(Plug.Conn.t(), map() | list()) :: Plug.Conn.t()
+      def json(conn, data) do
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(200, Jason.encode!(data))
+      end
+
+      @doc """
+      Sends a text response with the given status code and text.
+      ## Examples
+      ```
+      text(conn, 200, "Hello World!")
+      ```
+      """
+      @spec text(Plug.Conn.t(), integer(), String.t()) :: Plug.Conn.t()
+      def text(conn, status, text) do
+        conn
+        |> put_resp_content_type("text/plain")
+        |> send_resp(status, text)
+      end
+
+      @doc """
+      Sends a text response with a 200 status code and the given text.
+      ## Examples
+      ```
+      text(conn, "Hello World!")
+      ```
+      """
+      @spec text(Plug.Conn.t(), String.t()) :: Plug.Conn.t()
+      def text(conn, text) do
+        conn
+        |> put_resp_content_type("text/plain")
+        |> send_resp(200, text)
+      end
+
+      @doc """
+      Sends an HTML response with the given status code and HTML content.
+      ## Examples
+      ```
+      html(conn, 200, "<h1>Hello World!</h1>")
+      ```
+      """
+      @spec html(Plug.Conn.t(), integer(), String.t()) :: Plug.Conn.t()
+      def html(conn, status, html) do
+        conn
+        |> put_resp_content_type("text/html")
+        |> send_resp(status, html)
+      end
+
+      @doc """
+      Sends an HTML response with a 200 status code and the given HTML content.
+      ## Examples
+      ```
+      html(conn, "<h1>Hello World!</h1>")
+      ```
+      """
+      def html(conn, html) do
+        conn
+        |> put_resp_content_type("text/html")
+        |> send_resp(200, html)
+      end
+
       @spec handle_errors(Plug.Conn.t(), any()) :: Plug.Conn.t()
       @impl true
       def handle_errors(conn, reason) do
@@ -135,6 +218,10 @@ defmodule Francis do
             Keyword.get(unquote(opts), :error_handler).(conn, reason)
         end
       rescue
+        e in FunctionClauseError ->
+          Logger.error("Unhandled error: #{inspect(e)}")
+          send_resp(conn, 500, "Internal Server Error")
+
         e ->
           Logger.error("Error occurred: #{inspect(e)}")
           send_resp(conn, 500, "Internal Server Error")
