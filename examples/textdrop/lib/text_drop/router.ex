b3ba562407e493cd5a40889205fa68dda17a984a
Nelson Estevão <nelsonmestevao@proton.me>
2025-07-23 19:11:51 +0100

    chore: fix Dockerfile for deployment of textdrop including port

--- examples/textdrop/lib/text_drop/router.ex
+++ examples/textdrop/lib/text_drop/router.ex
@@ -1,5 +1,5 @@
 defmodule TextDrop.Router do
-  use Francis, static: [from: "priv/static", at: "/assets", gzip: true], bandit_opts: [port: 5050]
+  use Francis, static: [from: "priv/static", at: "/assets", gzip: true], bandit_opts: [port: 4000]
 
   get("/health", fn _ -> "OK" end)
 
